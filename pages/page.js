e5f8a81e0c51394886facd6b06e5ceee83ca0299
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-11-10 11:14:28 +0800

    3 files modified, 3 added

---
+++ pages/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrendingSlider from '../components/TrendingSlider';
+
+const Home = () => {
+  return (
+    <div>
+      <TrendingSlider />
+    </div>
+  );
+};
+
+export default Home;
