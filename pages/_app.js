1eb2cf064f2749ba388fd0562db8ead22ecfb3bf
rey5kusuma <96479873+rey5kusuma@users.noreply.github.com>
2023-11-10 13:15:09 +0800

    4 files modified, 8 added, 3 deleted

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+// import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
 
-export default MyApp
+// export default MyApp;
+
+/* eslint-disable react/no-danger */
+
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps }) => (
+  <>
+    <Component {...pageProps} />
+  </>
+);
+
+App.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.object,
+};
+
+export default App;
